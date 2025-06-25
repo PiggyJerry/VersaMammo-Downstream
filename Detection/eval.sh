07ff1d0ff816b06ce9584a788594488a864ce4e2
Jiayi Zhu <107994371+PiggyJerry@users.noreply.github.com>
2025-06-25 20:15:39 +0800

    Update eval.sh

--- Detection/eval.sh
+++ Detection/eval.sh
@@ -3,17 +3,17 @@
 PYTHON_FILE="/Detection/eval.py"
 
 #
-sed -i "s/hypar\['dataset'\]='.*'/hypar['dataset']='CBIS-DDSM-split'/" $PYTHON_FILE
+sed -i "s/hypar\['dataset'\]='.*'/hypar['dataset']='CBIS-DDSM'/" $PYTHON_FILE
 sed -i "s/hypar\['gpu_id'\]=\[.*\]/hypar['gpu_id']=[3]/" $PYTHON_FILE
 python $PYTHON_FILE & 
 sleep 30
 #
-sed -i "s/hypar\['dataset'\]='.*'/hypar['dataset']='INbreast-split'/" $PYTHON_FILE
+sed -i "s/hypar\['dataset'\]='.*'/hypar['dataset']='INbreast'/" $PYTHON_FILE
 sed -i "s/hypar\['gpu_id'\]=\[.*\]/hypar['gpu_id']=[3]/" $PYTHON_FILE
 python $PYTHON_FILE & 
 sleep 30
 #
-sed -i "s/hypar\['dataset'\]='.*'/hypar['dataset']='VinDr-Mammo-split'/" $PYTHON_FILE
+sed -i "s/hypar\['dataset'\]='.*'/hypar['dataset']='VinDr-Mammo'/" $PYTHON_FILE
 sed -i "s/hypar\['gpu_id'\]=\[.*\]/hypar['gpu_id']=[3]/" $PYTHON_FILE
 python $PYTHON_FILE & 
 sleep 30
